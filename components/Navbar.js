fb13381dfddc7a613968e4d63108faa7013f286a
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-12-28 03:02:00 +0700

    update for login condition, but not with search bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,28 @@
 import Image from 'next/image';
 import React, { useState } from 'react';
 import LogoSVG from '../public/assets/images/main-logo-color.svg'
+import userPicProfile from '../public/assets/images/user-profile.jpg'
+import { Popover } from '@headlessui/react'
+import { usePopper } from 'react-popper'
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+    const Login = false;
+
+    const [referenceElement, setReferenceElement] = useState()
+    const [popperElement, setPopperElement] = useState()
+    const { styles, attributes } = usePopper(referenceElement, popperElement, {
+        placement: 'bottom-end',
+    })
+    
+
     return (
         <>
-            <section className="py-8">
+            <section className="py-5">
                 <div className="container px-4 mx-auto">
                     <nav className="relative">
                         <div className="flex justify-between items-center">
-                            <a className="text-lg font-medium" href="#">
-                                {/* <img className="h-7" src="" alt="" width="auto" /> */}
+                            <a className="text-lg font-medium" href="/">
                                 <Image className="h-7" src={LogoSVG} alt="" width={120} height={30} />
                             </a>
                             <div className="lg:hidden">
@@ -28,7 +39,52 @@ export default function Navbar() {
                                 <li><a className="text-sm font-medium" href="#">Services</a></li>
                                 <li><a className="text-sm font-medium" href="#">Testimonials</a></li>
                             </ul>
-                            <div className="hidden lg:block"><a className="inline-block mr-2 py-3 px-8 text-sm leading-normal rounded border font-medium" href="#">Log in</a><a className="inline-block py-3 px-8 text-sm text-white font-medium leading-normal bg-red-400 hover:bg-red-300 rounded transition duration-200" href="#">Sign Up</a></div>
+                            {Login ? (
+                                <div className="hidden lg:block">
+                                    <a className="inline-block mr-2 py-3 px-8 text-sm leading-normal rounded border font-medium" href="#">Log in</a>
+                                    <a className="inline-block py-3 px-8 text-sm text-white font-medium leading-normal bg-red-400 hover:bg-red-300 rounded transition duration-200" href="#">Sign Up</a>
+                                </div>
+                            ) : (
+                                <div className="hidden lg:block">
+                                    <div className="grid grid-rows-2 grid-flow-col">
+                                        <div className="col-span-2">
+                                            <a className='text-sm font-medium'>Selamet Riyadi</a>
+                                        </div>
+                                        <div className="col-span-2">
+                                            <a className='text-sm'>Customer</a>
+                                        </div>
+                                        <div className="row-span-2 col-span-2 px-3 mt-2">
+                                             <Popover className="relative">
+                                                <Popover.Button 
+                                                    ref={setReferenceElement}>
+                                                    <Image
+                                                        className='rounded-full'
+                                                        src={userPicProfile}    
+                                                        height={35}
+                                                        width={35}
+                                                        alt=''
+                                                    />
+                                                </Popover.Button>
+                                                <Popover.Panel 
+                                                    ref={setPopperElement}
+                                                    style={styles.popper}
+                                                    {...attributes.popper}
+                                                    className="absolute w-36 max-w-sm pr-20 px-4 mt-3 sm:px-0 lg:max-w-3xl">
+                                                        <div className="overflow-hidden rounded-lg shadow-lg">
+                                                            <div className="relative grid bg-white p-3 lg:grid-cols-2">
+                                                                <a className='text-sm font-medium' href='/'>Live Chat</a>
+                                                            </div>
+                                                            <div className="relative grid bg-white p-3 lg:grid-cols-2 text-black bg-red-300 hover:bg-red-600 text-white">
+                                                                <a className='text-sm font-medium' href='/'>Log Out</a>
+                                                            </div>
+                                                    </div>
+                                                </Popover.Panel>
+                                                </Popover>
+                                        </div>
+                                    </div>
+                                </div>
+                                // <h1>ABC</h1>
+                            )}
                         </div>
                     </nav>
                 </div>
@@ -54,7 +110,10 @@ export default function Navbar() {
                             </ul>
                         </div>
                         <div className="mt-auto">
-                            <div className="pt-6"><a className="block mb-2 py-3 text-sm text-center leading-normal rounded border font-medium" href="#">Log in</a><a className="block py-3 text-sm text-center text-white leading-normal rounded bg-red-400 hover:bg-red-300 font-medium transition duration-200" href="#">Sign Up</a></div>
+                            <div className="pt-6">
+                                <a className="block mb-2 py-3 text-sm text-center leading-normal rounded border font-medium" href="#">Log in</a>
+                                <a className="block py-3 text-sm text-center text-white leading-normal rounded bg-red-400 hover:bg-red-300 font-medium transition duration-200" href="#">Sign Up</a>
+                            </div>
                             <p className="mt-6 mb-4 text-sm text-center text-gray-500">
                                 <span>© 2021 All rights reserved.</span>
                             </p>
@@ -65,3 +124,4 @@ export default function Navbar() {
         </>
     )
 }
+
