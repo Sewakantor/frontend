86e3688fa30c9f30eaaebc06f94ec40df42ac8a1
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2022-01-07 03:10:30 +0700

    [ADD] form validation for login and register 50%

--- components/FormRegister.js
+++ components/FormRegister.js
@@ -2,8 +2,26 @@ import Image from 'next/image'
 import headerImage from '../public/assets/images/header-image.jpg'
 import { ArrowCircleLeftIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
 
 export default function FormRegister() {
+    const {register, handleSubmit, formState: {errors} } = useForm();
+
+    // Regex on Validation
+    const regexEmail = /^\S+@\S+$/i;
+    const regexPassword = /\S{6,}$/;
+    const regexTelp = /^[0-9]{10,15}$/;
+    
+    const onSubmit = (data) => {
+        console.log(data);
+        Swal.fire(
+            'Good job!',
+            'You clicked the button!',
+            'success'
+        )
+        reset();
+    }
+
     return (
         <section className="overflow-hidden">
             <div className="flex min-h-screen overflow-hidden">
@@ -27,108 +45,79 @@ export default function FormRegister() {
                         </div>
                         <div className="mt-3">
                             <div className="">
-                                <form action="#" method="POST" className="space-y-2">
+                                <form action="#" method="POST" className="space-y-2" onSubmit={handleSubmit(onSubmit)}>
                                 <div>
                                     <label htmlFor="name" className="mb-2 block text-sm font-medium text-neutral-600"> Name </label>
                                         <div className="mt-1">
-                                            <input id="name" name="name" type="name" autoComplete="name" required placeholder="Your name" className="
-                                                block
-                                                w-full
-                                                px-3
-                                                py-2
-                                                text-base text-neutral-600
-                                                placeholder-gray-400
-                                                transition
-                                                duration-500
-                                                ease-in-out
-                                                transform
-                                                border border-transparent
-                                                rounded-lg
-                                                bg-[#EDEDED]
-                                                outline-none
-                                            " />
+                                            <input 
+                                                id="name" 
+                                                name="name" 
+                                                type="name" 
+                                                autoComplete="name" 
+                                                placeholder="Your name" 
+                                                className="block w-full px-3 py-2 text-base text-neutral-600 placeholder-gray-400 transition duration-500 ease-in-out transform rounded-lg bg-[#EDEDED] outline-none"
+                                                {...register("name", {required: "Nama tidak boleh kosong"})}
+                                            />
+                                            {errors.name && errors.name.type === "required" && <p className="mt-1 text-red-600 text-xs font-medium">{errors.name.message}</p>}
                                         </div>
                                       </div>
                                     <div className="space-y-1">
                                     <label htmlFor="email" className="mb-2 block text-sm font-medium text-neutral-600"> Email Address </label>
-                                            <input id="email" name="email" type="email" autoComplete="email" required placeholder="Your email" className="
-                                                block
-                                                w-full
-                                                px-3
-                                                py-2
-                                                text-base text-neutral-600
-                                                placeholder-gray-400
-                                                transition
-                                                duration-500
-                                                ease-in-out
-                                                transform
-                                                border border-transparent
-                                                rounded-lg
-                                                bg-[#EDEDED]
-                                                outline-none
-                                            " />
+                                            <input 
+                                                id="email" 
+                                                name="email" 
+                                                type="email" 
+                                                autoComplete="email" 
+                                                placeholder="Your email" 
+                                                className="block w-full px-3 py-2 text-base text-neutral-600 placeholder-gray-400 transition duration-500 ease-in-out transform rounded-lg bg-[#EDEDED] outline-none"
+                                                {...register("email", {required: "Email tidak boleh kosong", pattern: regexEmail})} 
+                                            />
+                                            {errors.email && errors.email.type === "required" && <p className="mt-1 text-red-600 text-xs font-medium">{errors.email.message}</p>}
+                                            {errors.email && errors.email.type === "pattern" && <p className="mt-1 text-red-600 text-xs font-medium">Format email tidak valid</p>}
                                     </div>
                                     <div className="space-y-1">
                                     <label htmlFor="password" className="mb-2 block text-sm font-medium text-neutral-600"> Password </label>
                                         <div className="mt-1">
-                                            <input id="password" name="name" type="password" autoComplete="password" required placeholder="Password" className="
-                                                block
-                                                w-full
-                                                px-3
-                                                py-2
-                                                text-base text-neutral-600
-                                                placeholder-gray-400
-                                                transition
-                                                duration-500
-                                                ease-in-out
-                                                transform
-                                                border border-transparent
-                                                rounded-lg
-                                                bg-[#EDEDED]
-                                                outline-none
-                                            " />
+                                            <input 
+                                                id="password" 
+                                                name="name" 
+                                                type="password" 
+                                                autoComplete="password" 
+                                                placeholder="Password" 
+                                                className="block w-full px-3 py-2 text-base text-neutral-600 placeholder-gray-400 transition duration-500 ease-in-out transform rounded-lg bg-[#EDEDED] outline-none"
+                                                {...register("password",{required:"Password tidak boleh kosong", pattern: regexPassword})}                                           
+                                            />
+                                            {errors.password && errors.password.type === "required" && <p className="mt-1 text-red-600 text-xs font-medium">{errors.password.message}</p>}
+                                            {errors.password && errors.password.type === "pattern" && <p className="mt-1 text-red-600 text-xs font-medium">Format password min 6 digit dan tidak ada spasi.</p>}                                        
                                         </div>
                                     </div>
                                     <div className="space-y-1">
                                     <label htmlFor="text" className="mb-2 block text-sm font-medium text-neutral-600"> Company </label>
                                         <div className="mt-1">
-                                            <input id="company" name="company" type="text" autoComplete="company" required placeholder="Company" className="
-                                                block
-                                                w-full
-                                                px-3
-                                                py-2
-                                                text-base text-neutral-600
-                                                placeholder-gray-400
-                                                transition
-                                                duration-500
-                                                ease-in-out
-                                                transform
-                                                border border-transparent
-                                                rounded-lg
-                                                bg-[#EDEDED]
-                                                outline-none
-                                            " />
+                                            <input 
+                                                id="company" 
+                                                name="company" 
+                                                type="text" 
+                                                autoComplete="company" 
+                                                placeholder="Company" 
+                                                className="block w-full px-3 py-2 text-base text-neutral-600 placeholder-gray-400 transition duration-500 ease-in-out transform rounded-lg bg-[#EDEDED] outline-none"
+                                                {...register("company",{required:"Asal perusahaan tidak boleh kosong"})}
+                                            />
+                                            {errors.company && errors.company.type === "required" && <p className="mt-1 text-red-600 text-xs font-medium">{errors.company.message}</p>}
                                         </div>
                                     </div>
                                     <div className="space-y-1">
-                                    <label htmlFor="tel" className="mb-2 block text-sm font-medium text-neutral-600"> Name </label>
+                                    <label htmlFor="tel" className="mb-2 block text-sm font-medium text-neutral-600"> No. Handphone </label>
                                         <div className="mt-1">
-                                            <input id="tel" name="tel" type="tel" autoComplete="telphone" required placeholder="+62" className="
-                                                block
-                                                w-full
-                                                px-3
-                                                py-2
-                                                text-base text-neutral-600
-                                                placeholder-gray-400
-                                                transition
-                                                duration-500
-                                                ease-in-out
-                                                transform
-                                                border border-transparent
-                                                rounded-lg
-                                                bg-[#EDEDED]
-                                                outline-none
-                                            " />
+                                            <input 
+                                                id="nohandphone" 
+                                                name="nohandphone" 
+                                                type="number" 
+                                                autoComplete="telphone" 
+                                                placeholder="+62" 
+                                                className="" 
+                                                {...register("nohandphone")}
+                                            />
                                         </div>
                                     </div>
                                     <div className="my-10 text-sm text-right">
