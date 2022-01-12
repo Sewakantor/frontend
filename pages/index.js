c7e3afe8bd3258d7deba0a07c6d8450867de7540
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2022-01-12 16:00:19 +0700

    [FIX] Clearing on index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import CardOffice from "../components/CardOffice";
-import HeaderOnDetail from "../components/HeaderOnDetail";
-
 export default function Home() {
   return (
     <>
-      <HeaderOnDetail/>
+
     </>
   )
 }
