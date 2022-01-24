64353d279be10969d265c305ba0cfe8a8090849a
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2022-01-24 14:27:02 +0700

    [SYNC] BE Get Detail Building

--- components/AboutOnDetail.js
+++ components/AboutOnDetail.js
@@ -1,15 +1,14 @@
-export default function AboutOnDetail() {
+import { data } from "autoprefixer";
+
+export default function AboutOnDetail({data}) {
+
     return (
         <div className="">
             <div className="">
                 <div className="">
-                    <h1 className="mb-5 font-bold text-2xl">Tentang Wisma Atlet</h1>
+                    <h1 className="mb-5 font-bold text-2xl">Tentang {data.Name} </h1>
                     <p className="whitespace-pre-line text-lg">
-                        Wisma Atlet adalah bangunan kantor baru tipe Grade A yang terletak di Central Business District (Pusat Kawasan Bisnis), jantung kota Jakarta. Dengan total luas bangunan 32.000 m2, bangunan kantor beralamat di Jl. Kemayoran, Jakarta Pusat tersedia untuk sewa.
-                        
-                        Wisma Atlet memiliki 400 tempat parkir yang dapat digunakan oleh tenan dan tamu serta ATM dan Bank sebagai fasilitas di kawasan kantor.
-                    
-                        Wisma Atlet adalah bangunan 28 lantai jarak dengan berjalan kaki Kuningan City, Mal Ambassador dan ITC Kuningan. Wisma Atlet yang dilengkapi dengan 12 lift yang dibagi menjadi 2 zona, dikenal dengan Low Zone dan High Zone, dan dedicated service lift dan executive lift.
+                        {data.Desc}
                     </p>
                 </div>
                 <div className="mt-8">
