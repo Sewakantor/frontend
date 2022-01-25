8ea1ac6bd246e9d39e6a6e315339e453b46f884a
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2022-01-25 15:37:36 +0700

    [ADD] Search Page

--- components/CardOffice.js
+++ components/CardOffice.js
@@ -1,21 +1,21 @@
 import Image from 'next/image'
 import dummyImage from '../public/assets/images/card-kantor-dummy-image.jpg'
 
-export default function CardOffice() {
+export default function CardOffice({data}) {
     return (
-        <div className="w-64 rounded overflow-hidden shadow-lg">
+        <div className="w-72 rounded overflow-hidden shadow-lg">
             <div className="h-80 max-w-full relative">
-                <Image className='absolute rounded-md object-none' layout="fill" src={dummyImage} alt="Sunset in the mountains" objectFit='cover'/>
+                <Image className='absolute rounded-md object-none' layout="fill" src={data.Photo} alt="Sunset in the mountains" objectFit='cover'/>
             </div>
             <div className="px-4 py-2 pb-8">
                 <div className="font-semibold text-gray-700 text-xl mb-2">
-                    Wisma Atlet
+                    {data.Name}
                     </div>
                 <p className="text-sm font-light text-gray-700">
                     Mulai dari
                 </p>
                 <p className="font-semibold text-gray-700">
-                    Rp 200.000 / sqm / bulan
+                    Rp. {data.Price} / sqm / bulan
                 </p>
             </div>
         </div>
